61215b390f8cafb5b0f337c677ffe04dd1a595d6
João Henrique <139931834+bkr6@users.noreply.github.com>
2025-09-07 21:21:00 -0300

    Mapeamento de entidades

---
+++ libraryapi/libraryapi/src/main/java/projeto/lazzy/teste/libraryapi/model/Livro.java
@@ -0,0 +1,44 @@
+package projeto.lazzy.teste.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+public class Livro {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "isbn", length = 20, nullable = false)
+    private String isbn;
+
+    @Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+
+    @Column(name = "data_publicacao", nullable = false)
+    private LocalDate dataPublicacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genero", length = 30, nullable = false)
+    private GeneroLivro genero;
+
+    @Column(name = "preco", precision = 18, scale = 2)
+    private BigDecimal preco;
+
+    @JoinColumn(name = "id_autor")
+    @ManyToOne
+    private Autor autor;
+}
