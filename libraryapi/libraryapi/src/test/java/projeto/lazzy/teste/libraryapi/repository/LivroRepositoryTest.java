5e7d425835bcdd5c0327e7d95142e8c616901c78
João Reinaldo <jreinaldo@abbc.org.br>
2025-09-11 16:23:21 -0300

    Arrumando alguns testes

--- libraryapi/libraryapi/src/test/java/projeto/lazzy/teste/libraryapi/repository/LivroRepositoryTest.java
+++ libraryapi/libraryapi/src/test/java/projeto/lazzy/teste/libraryapi/repository/LivroRepositoryTest.java
@@ -2,7 +2,12 @@ package projeto.lazzy.teste.libraryapi.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import projeto.lazzy.teste.libraryapi.model.Autor;
+import projeto.lazzy.teste.libraryapi.model.GeneroLivro;
 import projeto.lazzy.teste.libraryapi.model.Livro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +19,20 @@ class LivroRepositoryTest {
     @Test
     public void salvarTest(){
         Livro livro = new Livro();
+        livro.setIsbn("Slaaaaaaaaaaaa");
+        livro.setPreco(BigDecimal.valueOf(234.50));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("Abobrinhas");
+        livro.setDataPublicacao(LocalDate.of(1994,2,12));
 
+        Autor autor = new Autor();
+        autor.setName("Maria");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
     }
 
 }
